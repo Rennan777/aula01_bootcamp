c38d8c14e3e24f56f670eaedfcc214be61bc59f9
Rennan Souza <rennan777@gmail.com>
2024-10-03 20:47:25 -0300

    exercicio 2 e ajustes

---
+++ exemplo_02.py
@@ -0,0 +1,9 @@
+#Crie um programa onde o usuario digite 2 valores e apareca a soma
+
+valor1 = int(input("Digite o primeiro valor: "))
+
+valor2 = int(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+
+print(f"A soma de {valor1} e {valor2} é igual a {soma}.")
