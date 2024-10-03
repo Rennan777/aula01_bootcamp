8ca7a6b1381f6ba8bbd7849a6ff2ac876037bcca
Rennan Souza <rennan777@gmail.com>
2024-10-03 20:42:22 -0300

    exercicio 1

---
+++ exemplo_00.py
@@ -0,0 +1,6 @@
+#Crie um programa que o usuario digita seu nome e retorna o numero de caracteres
+
+nome = input("digite seu nome: ")
+tamanho = len(nome)
+
+print(f"Seu nome tem {tamanho} caracteres.")
