6b1bc0fc80fea10c66927d8e090f70d2a87724ac
Rennan Souza <rennan777@gmail.com>
2024-10-03 20:56:54 -0300

    exercicio 3

---
+++ exemplo_03.py
@@ -0,0 +1,9 @@
+#Refatore o exercicio 2 utilizando variaveis 
+
+valor1 = int(input("Digite o primeiro valor: "))
+
+valor2 = int(input("Digite o segundo valor: "))
+
+soma = valor1 + valor2
+
+print(f"A soma de {valor1} e {valor2} é igual a {soma}.")
